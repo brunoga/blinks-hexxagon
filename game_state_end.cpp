121d83f479835cbe62ac09db288022f14a00b6ab
Bruno Albuquerque <bga@bug-br.org.br>
2020-08-13 10:41:30 -0700

    Implement end state.
    - Untested but the idea is that all blinks will show the color(s) of the
      winner(s).

--- game_state_end.cpp
+++ game_state_end.cpp
@@ -1,5 +1,9 @@
 #include "game_state_end.h"
 
+#include "blink_state.h"
+#include "game_player.h"
+#include "game_state.h"
+
 namespace game {
 
 namespace state {
@@ -7,8 +11,33 @@ namespace state {
 namespace end {
 
 void Handler(bool state_changed, byte* state, byte* specific_state) {
-  // TODO(bga): Implement me.
-}
+  if (state_changed) {
+    byte max_count = 0;
+    byte num_players_max_count = 0;
+
+    byte players_max_count[GAME_PLAYER_MAX_PLAYERS];
+
+    for (byte i = 1; i < GAME_PLAYER_MAX_PLAYERS + 1; ++i) {
+      byte player_count = game::state::GetBlinkCount(i);
+      if (player_count >= max_count) {
+        if (player_count > max_count) {
+          max_count = player_count;
+          num_players_max_count = 1;
+        }
+
+        players_max_count[num_players_max_count - 1] = i;
+      }
+    }
+
+    blink::state::SetPlayer(
+        players_max_count[random(num_players_max_count - 1)]);
+  }
+
+  if (buttonSingleClicked()) {
+    *state = GAME_STATE_IDLE;
+    *specific_state = 0;
+  }
+}  // namespace end
 
 }  // namespace end
 
