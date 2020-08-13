cf3f100ea23fa700c4bc910bff0df627a2992f47
Bruno Albuquerque <bga@bug-br.org.br>
2020-08-13 10:10:46 -0700

    Fix minimum payload size.

--- payload_bytes.h
+++ payload_bytes.h
@@ -6,11 +6,11 @@
 // This is the maximum payload we will need which is 5 bytes for the
 // MESSAGE_CHECK_BOARD message (considering a maximum of 4 players). If we have
 // 3 players or less, then the biggest message is the
-// MESSAGE_GAME_STATE_PLAY_FIND_TARGETS with a payload of 3.
+// MESSAGE_GAME_STATE_PLAY_FIND_TARGETS with a payload of 4.
 #if GAME_PLAYER_MAX_PLAYERS > 2
 #define MESSAGE_PAYLOAD_BYTES GAME_PLAYER_MAX_PLAYERS + 1
 #else
-#define MESSAGE_PAYLOAD_BYTES 3
+#define MESSAGE_PAYLOAD_BYTES 4
 #endif
 
 #endif  // PAYLOAD_BYTES_H_
