07b830e61c3d5a3c2dff5d1fbeccef13a2ab987c
Bruno Albuquerque <bga@bug-br.org.br>
2020-08-28 16:04:29 -0700

    Datagram is not needed anymore.
    - Updated broadcast code uses Custom Blinklib.

--- datagram.cpp
+++
@@ -1,1 +0,0 @@
-../datagram/datagram.cpp
--- datagram_bytes.h
+++
@@ -1,9 +0,0 @@
-#ifndef DATAGRAM_BYTES_H_
-#define DATAGRAM_BYTES_H_
-
-#include "payload_bytes.h"
-
-// Max number of datagram bytes in a message.
-#define DATAGRAM_BYTES MESSAGE_PAYLOAD_BYTES + 1
-
-#endif
