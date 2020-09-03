f7716f3959988a24218284b29194be0c2d0b78c4
Bruno Albuquerque <bga@bug-br.org.br>
2020-09-03 10:25:14 -0700

    Fix sending end of game notification.
    - Back to 96% storage usage but it had to be done.

--- game_state_end.cpp
+++ game_state_end.cpp
@@ -11,38 +11,39 @@ namespace state {
 
 namespace end {
 
+static bool sent_flash_;
 static byte winner_player_;
 
 void Handler(bool state_changed, byte* state, byte* specific_state) {
-  (void)specific_state;
   (void)state;
+  (void)specific_state;
 
   if (!blink::state::GetTarget()) return;
 
   if (state_changed) {
-    // Tell all Blinks to flash. Should be ok to ignore the return value here.
-    // TODO(bga): Make sure it is or fix this.
-    game::message::SendFlash();
-
-    return;
-  }
-
-  byte max_count = 0;
-  for (byte i = 1; i < GAME_PLAYER_MAX_PLAYERS + 1; ++i) {
-    byte player_count = GetBlinkCount(i);
-    if (player_count > max_count) {
-      max_count = player_count;
-      winner_player_ = i;
-    } else if (player_count == max_count) {
-      winner_player_ = 0;
+    byte max_count = 0;
+    for (byte i = 1; i < GAME_PLAYER_MAX_PLAYERS + 1; ++i) {
+      byte player_count = GetBlinkCount(i);
+      if (player_count > max_count) {
+        max_count = player_count;
+        winner_player_ = i;
+      } else if (player_count == max_count) {
+        winner_player_ = 0;
+      }
     }
+
+    sent_flash_ = false;
   }
+
+  if (!sent_flash_ && !game::message::SendFlash()) return;
+
+  sent_flash_ = true;
 
   if (!game::message::SendReportWinner(winner_player_)) return;
 
   // Make sure we will not just continue sending messages.
   blink::state::SetTarget(false);
-}
+}  // namespace end
 
 }  // namespace end
 
