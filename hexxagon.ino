5319767b6f6ecb2fc42be76808913064bf0e23ba
Bruno Albuquerque <bga@bug-br.org.br>
2020-09-10 09:56:43 -0700

    Save another 6 bytes.

--- hexxagon.ino
+++ hexxagon.ino
@@ -18,49 +18,51 @@
 void setup() { game::message::Setup(); }
 
 void loop() {
-  if (!isAlone()) {
-    // Process any pending game messages.
-    game::message::Process();
-
-    // Check escape hatch. Reset to idle state if button is long pressed.
-    if (buttonLongPressed()) {
-      // Ok to ignore result.
-      game::message::SendFlash();
-
-      game::state::Set(GAME_STATE_IDLE);
-    }
-
-    if (game::state::Propagate()) {
-      // Cache current state and if we changed state since the previous
-      // iteration.
-      byte state = game::state::Get();
-      byte specific_state = game::state::GetSpecific();
-      bool state_changed = game::state::Changed(false);
-
-      // Run our state machine.
-      switch (state) {
-        case GAME_STATE_IDLE:
-          game::state::idle::Handler(state_changed, &state, &specific_state);
-          break;
-        case GAME_STATE_SETUP:
-          game::state::setup::Handler(state_changed, &state, &specific_state);
-          break;
-        case GAME_STATE_PLAY:
-          game::state::play::Handler(state_changed, &state, &specific_state);
-          break;
-        case GAME_STATE_END:
-          game::state::end::Handler(state_changed, &state, &specific_state);
-      }
-
-      // Switch our state to the computed one. This will be propagated to other
-      // Blinks in case there was a change.
-      game::state::Set(state);
-      game::state::SetSpecific(specific_state);
-    }
-
-    blink::state::Render(game::state::Get());
-  } else {
+  if (isAlone()) {
     // We are alone. Make it evident.
     render::animation::Pulse(WHITE, 255, 2);
+
+    return;
   }
+
+  // Process any pending game messages.
+  game::message::Process();
+
+  // Check escape hatch. Reset to idle state if button is long pressed.
+  if (buttonLongPressed()) {
+    // Ok to ignore result.
+    game::message::SendFlash();
+
+    game::state::Set(GAME_STATE_IDLE);
+  }
+
+  if (game::state::Propagate()) {
+    // Cache current state and if we changed state since the previous
+    // iteration.
+    byte state = game::state::Get();
+    byte specific_state = game::state::GetSpecific();
+    bool state_changed = game::state::Changed(false);
+
+    // Run our state machine.
+    switch (state) {
+      case GAME_STATE_IDLE:
+        game::state::idle::Handler(state_changed, &state, &specific_state);
+        break;
+      case GAME_STATE_SETUP:
+        game::state::setup::Handler(state_changed, &state, &specific_state);
+        break;
+      case GAME_STATE_PLAY:
+        game::state::play::Handler(state_changed, &state, &specific_state);
+        break;
+      case GAME_STATE_END:
+        game::state::end::Handler(state_changed, &state, &specific_state);
+    }
+
+    // Switch our state to the computed one. This will be propagated to other
+    // Blinks in case there was a change.
+    game::state::Set(state);
+    game::state::SetSpecific(specific_state);
+  }
+
+  blink::state::Render(game::state::Get());
 }
