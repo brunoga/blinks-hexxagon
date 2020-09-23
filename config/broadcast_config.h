7f194ff5fe0ae23b4ff4153e75805ede98075d2b
Bruno Albuquerque <bga@bug-br.org.br>
2020-09-23 06:27:27 -0700

    Use new config folder support.
    - Configure the broadcast module using it.
    - Shaved another 20 bytes!

---
+++ config/broadcast_config.h
@@ -0,0 +1,8 @@
+#ifndef BROADCAST_CONFIG_H_
+#define BROADCAST_CONFIG_H_
+
+// This is the maximum payload we will need which is 5 bytes for the
+// MESSAGE_CHECK_BOARD message.
+#define MESSAGE_PAYLOAD_BYTES 5
+
+#endif
