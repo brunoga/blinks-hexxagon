c31ef530e26faee6c2fe29faf94cb4f5f65a5822
Bruno Albuquerque <bga@bug-br.org.br>
2020-10-30 09:09:26 -0700

    Auto detect correct maximum payload size.

--- config/broadcast_config.h
+++ config/broadcast_config.h
@@ -1,8 +1,15 @@
 #ifndef BROADCAST_CONFIG_H_
 #define BROADCAST_CONFIG_H_
 
-// This is the maximum payload we will need which is 5 bytes for the
-// MESSAGE_CHECK_BOARD_STATE message.
-#define MESSAGE_PAYLOAD_BYTES 5
+#include "../game_player.h"
+
+// This is the maximum payload we will need.
+#if (GAME_PLAYER_MAX_PLAYERS + 1) > 4
+// MESSAGE_CHECK_BOARD_STATE/MESSAGE_REPORT_BOARD_STATE
+#define MESSAGE_PAYLOAD_BYTES GAME_PLAYER_MAX_PLAYERS + 1
+#else
+// MESSAGE_
+#define MESSAGE_PAYLOAD_BYTES 4
+#endif
 
 #endif
