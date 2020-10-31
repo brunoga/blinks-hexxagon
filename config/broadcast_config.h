f30d3655eac93974454aaf36db1ba8808f2decfd
Bruno Albuquerque <bga@bug-br.org.br>
2020-10-30 21:50:10 -0700

    Simplify setting MESSAGE_PAYLOAD_BYTES.
    - We have a minimum of 2 players so the smallest payload would be 3.

--- config/broadcast_config.h
+++ config/broadcast_config.h
@@ -4,12 +4,6 @@
 #include "../game_player.h"
 
 // This is the maximum payload we will need.
-#if (GAME_PLAYER_MAX_PLAYERS + 1) > 4
-// MESSAGE_CHECK_BOARD_STATE/MESSAGE_REPORT_BOARD_STATE
 #define MESSAGE_PAYLOAD_BYTES GAME_PLAYER_MAX_PLAYERS + 1
-#else
-// MESSAGE_
-#define MESSAGE_PAYLOAD_BYTES 4
-#endif
 
 #endif
