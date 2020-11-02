128e4059c8ea1f0c00dd338f753d1faf5bd18602
Bruno Albuquerque <bga@bug-br.org.br>
2020-11-01 17:39:38 -0800

    Revert "Reenable lightning animation for now."
    This reverts commit 00d21b8ef92615bbb21ccd359ed15951a7497261.

--- config/hexxagon_config.h
+++ config/hexxagon_config.h
@@ -1,8 +1,6 @@
 #ifndef HEXXAGON_CONFIG_H_
 #define HEXXAGON_CONFIG_H_
 
-// Uncomment the line below to disable the lightning animation and save some
-// storage space.
-//#define HEXXAGON_DISABLE_LIGHTNING_ANIMATION
+#define HEXXAGON_DISABLE_LIGHTNING_ANIMATION
 
 #endif
