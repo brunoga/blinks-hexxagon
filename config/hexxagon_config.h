00d21b8ef92615bbb21ccd359ed15951a7497261
Bruno Albuquerque <bga@bug-br.org.br>
2020-11-01 17:21:07 -0800

    Reenable lightning animation for now.

--- config/hexxagon_config.h
+++ config/hexxagon_config.h
@@ -1,6 +1,8 @@
 #ifndef HEXXAGON_CONFIG_H_
 #define HEXXAGON_CONFIG_H_
 
-#define HEXXAGON_DISABLE_LIGHTNING_ANIMATION
+// Uncomment the line below to disable the lightning animation and save some
+// storage space.
+//#define HEXXAGON_DISABLE_LIGHTNING_ANIMATION
 
 #endif
