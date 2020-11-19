f81250936c9b555fb5173ec493f501ff2eab8a82
Bruno Albuquerque <bga@gmail.com>
2020-11-18 17:02:45 -0800

    Add initial game map implementation.
    - Not hooked up yet. As soon as it is, we will see space exploding.
    - Used both to create a map and to operate on it (currently only
      searching for empty spaces in range.
    - Unfortunately all operations have to be O(n) as we can not use
      anything fancier than an array.
    - And no, we can not even afford to sort the array (not enough memory).
    - This will really push memory usage to its limits.

---
+++ game_map.cpp
@@ -0,0 +1,117 @@
+#include "game_map.h"
+
+#include "blink_state.h"
+#include "game_state.h"
+#include "src/blinks-orientation/orientation.h"
+#include "src/blinks-position/position.h"
+
+#define GAME_MAP_MAX_BLINKS 128
+
+namespace game {
+
+namespace map {
+
+struct Data {
+  position::Coordinates coordinates;
+  byte player;
+};
+
+static Data map_[GAME_MAP_MAX_BLINKS];
+static int8_t last_received_index_ = -1;
+static int8_t last_propagated_index_ = -1;
+
+static bool initialized_;
+
+static void propagate(const position::Coordinates& coordinates, byte player) {
+  FOREACH_FACE(face) {
+    if (isValueReceivedOnFaceExpired(face)) continue;
+
+    byte datagram[4] = {orientation::RelativeLocalFace(face),
+                        (byte)coordinates.x, (byte)coordinates.y, player};
+
+    sendDatagramOnFace(datagram, 4, face);
+  }
+}
+
+static bool is_new_data(int8_t x, int8_t y) {
+  if (x == position::Local().x && y == position::Local().y) {
+    return false;
+  }
+
+  for (byte i = 0; i < GAME_MAP_MAX_BLINKS; ++i) {
+    if (map_[i].coordinates.x == POSITION_INVALID_COORDINATE) break;
+
+    if (x == map_[i].coordinates.x && y == map_[i].coordinates.y) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void Process() {
+  // Read any pending datagram and queue new information.
+  FOREACH_FACE(face) {
+    if (getDatagramLengthOnFace(face) != 4) continue;
+
+    const byte* datagram = getDatagramOnFace(face);
+
+    if (!initialized_) {
+      orientation::Setup(datagram[0], face);
+      position::Setup(datagram[0], datagram[1], datagram[2]);
+
+      propagate(position::Local(), blink::state::GetPlayer());
+
+      initialized_ = true;
+    }
+
+    if (!is_new_data(datagram[1], datagram[2])) continue;
+
+    last_received_index_++;
+    map_[last_received_index_].coordinates =
+        position::Coordinates{datagram[1], datagram[2]};
+    map_[last_received_index_].player = datagram[3];
+  }
+
+  if (isDatagramPendingOnAnyFace()) {
+    // Wait until all pending datagrams are sent to continue processing any
+    // other data.
+    return;
+  }
+
+  if (last_propagated_index_ < last_received_index_) {
+    // Forward a single new pending coordinate. This together with the check
+    // above guarantees that sending will never fail.
+    last_propagated_index_++;
+
+    propagate(map_[last_propagated_index_].coordinates,
+              map_[last_propagated_index_].player);
+  }
+}
+
+void StartMapping() {
+  // We are the origin of the coordinates system. Reset position and
+  // orientation and propagate new values.
+  orientation::Reset();
+  position::Reset();
+
+  propagate(position::Local(), blink::state::GetPlayer());
+
+  initialized_ = true;
+}
+
+void StopMapping() { game::state::SetMapping(false); }
+
+bool EmptySpaceInRange() {
+  for (byte i = 0; i < last_received_index_; ++i) {
+    if (map_[i].player == 0 && position::Distance(map_[i].coordinates) <= 2) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+}  // namespace map
+
+}  // namespace game
---
+++ game_map.h
@@ -0,0 +1,28 @@
+#ifndef GAME_MAP_H_
+#define GAME_MAP_H_
+
+namespace game {
+
+namespace map {
+
+// Process should be called at the top of the mapping loop and will handle all
+// incoming mapping messages.
+void Process();
+
+// Starts the mapping process. The Blink that calls it will be the coordinate
+// system origin (0, 0, 0). Only a single Blink in the cluster should call this
+// otherwise the behavior will be undefined and will probably break everything
+// badly.
+void StartMapping();
+
+// Stops the mapping process. Should be called by the origin Blink.
+void StopMapping();
+
+// Returns true if there is an empty space in range of the local Blink.
+bool EmptySpaceInRange();
+
+}  // namespace map
+
+}  // namespace game
+
+#endif
