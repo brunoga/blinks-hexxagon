e2e02596192e02f700061e3a4426e7d290cbbf3c
Bruno Albuquerque <bga@gmail.com>
2020-12-14 06:50:50 -0800

    Cleanup.
    - And saved 2 bytes of storage.

--- game_state_play_render.cpp
+++ game_state_play_render.cpp
@@ -21,36 +21,42 @@ void Render() {
   byte player = blink::state::GetPlayer();
   Color player_color = game::player::GetColor(player);
 
+  byte game_player = game::state::GetPlayer();
+
   // Set initial color for this Blink. This is required as some of the render
   // animations only render an overlay.
   setColor(player_color);
 
-  if ((game::state::GetSpecific() == GAME_STATE_PLAY_MOVE_CONFIRMED) &&
-      (blink::state::GetPlayer() != 0) &&
-      (blink::state::GetPlayer() != game::state::GetPlayer()) &&
-      position::Distance(game::map::GetMoveTarget()) == 1) {
-    // Render takeover (explosion) animation.
-    if (render::animation::Explosion(player_color)) {
-      blink::state::SetPlayer(game::state::GetPlayer());
+  if (player == 0) {
+    if (blink::state::GetTargetType() == BLINK_STATE_TARGET_TYPE_NONE) {
+      setColor(dim(player_color, RENDER_CONFIG_PLAY_STATE_COLOR_DIM));
     }
-  } else if (blink::state::GetOrigin() &&
-             game::state::GetSpecific() < GAME_STATE_PLAY_TARGET_SELECTED) {
-    // We are the Origin. Render the spinning animation.
-    render::animation::Spinner(RENDER_CONFIG_PLAY_STATE_SPINNER_COLOR,
-                               RENDER_CONFIG_PLAY_STATE_SPINNER_SLOWDOWN);
-  } else if ((game::state::GetPlayer() == player) &&
-             (game::state::GetSpecific() < GAME_STATE_PLAY_MOVE_CONFIRMED) &&
-             game::map::GetStatistics().local_blink_empty_space_in_range) {
-    // This Blink belongs to the current player and did not match any of the
-    // above conditions. Render a pulsing animation if we are not confirming the
-    // move yet.
-    render::animation::Pulse(player_color,
-                             RENDER_CONFIG_PLAY_STATE_PULSE_START_DIM,
-                             RENDER_CONFIG_PLAY_STATE_PULSE_SLOWDOWN);
-  } else if (blink::state::GetTargetType() == BLINK_STATE_TARGET_TYPE_NONE) {
-    // We did not match anything above and we are not a target. Render a dimmed
-    // version of the player color.
-    setColor(dim(player_color, RENDER_CONFIG_PLAY_STATE_COLOR_DIM));
+  } else {
+    if (player == game_player) {
+      if (blink::state::GetOrigin() &&
+          game::state::GetSpecific() < GAME_STATE_PLAY_TARGET_SELECTED) {
+        // We are the Origin. Render the spinning animation.
+        render::animation::Spinner(RENDER_CONFIG_PLAY_STATE_SPINNER_COLOR,
+                                   RENDER_CONFIG_PLAY_STATE_SPINNER_SLOWDOWN);
+      } else if ((game::state::GetSpecific() <
+                  GAME_STATE_PLAY_MOVE_CONFIRMED) &&
+                 game::map::GetStatistics().local_blink_empty_space_in_range) {
+        // This Blink belongs to the current player and did not match any of the
+        // above conditions. Render a pulsing animation if we are not confirming
+        // the move yet.
+        render::animation::Pulse(player_color,
+                                 RENDER_CONFIG_PLAY_STATE_PULSE_START_DIM,
+                                 RENDER_CONFIG_PLAY_STATE_PULSE_SLOWDOWN);
+      }
+    } else {
+      if ((game::state::GetSpecific() == GAME_STATE_PLAY_MOVE_CONFIRMED) &&
+          position::Distance(game::map::GetMoveTarget()) == 1) {
+        // Render takeover (explosion) animation.
+        if (render::animation::Explosion(player_color)) {
+          blink::state::SetPlayer(game_player);
+        }
+      }
+    }
   }
 }
 
