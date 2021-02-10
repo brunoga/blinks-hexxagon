d5e85e245dc0c5219f147e9c0d30daeab21f729c
Bruno Albuquerque <bga@gmail.com>
2021-02-10 15:20:14 -0800

    Change transmission timeouts.
    - This is a new feature in the custom Blinklib.
    - Required due to the amount of data being sent to all connected faces.

--- config/blinklib_config.h
+++ config/blinklib_config.h
@@ -5,4 +5,10 @@
 // of memory.
 #define BGA_CUSTOM_BLINKLIB_IR_DATAGRAM_LEN 5
 
+// Set data transmission timeouts. Due to the number of datagrams being sent in
+// multiple faces, the default values are not enough fot Hexxagon, so we
+// increase them by 100 ms here.
+#define BGA_CUSTOM_BLINKLIB_SEND_PROBE_TIMEOUT_MS 250
+#define BGA_CUSTOM_BLINKLIB_FACE_EXPIRATION_TIMEOUT_MS 300
+
 #endif
