3f4852aa83305a519b1e80dffbab41f1ca54a15f
Bruno Albuquerque <bga@gmail.com>
2021-02-14 07:38:59 -0800

    Revert back to the previous GetNext().
    - The new one was both broken and used 2 extra bytes.

--- game_player.cpp
+++ game_player.cpp
@@ -8,7 +8,9 @@ static const Color colors_[] = {MAKECOLOR_5BIT_RGB(31, 25, 0), RED, BLUE, GREEN,
                                 MAGENTA};
 
 byte __attribute__((noinline)) GetNext(byte current_player) {
-  return (current_player + 1) % GAME_PLAYER_MAX_PLAYERS;
+  byte next_player = current_player + 1;
+
+  return next_player > GAME_PLAYER_MAX_PLAYERS ? 0 : next_player;
 }
 
 Color GetColor(byte player) { return colors_[player]; }
