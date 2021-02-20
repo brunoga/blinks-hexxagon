8825894b06ce318e4fa575fb86a0953d024c97f0
Bruno Albuquerque <bga@gmail.com>
2021-02-20 06:43:22 -0800

    Just use the new colors. No other change.
    - There are some issues that will need to be dealt with.

--- game_player.cpp
+++ game_player.cpp
@@ -1,11 +1,15 @@
 #include "game_player.h"
 
+#define MAPLE MAKECOLOR_5BIT_RGB(31, 6, 0)     // red/orange
+#define BIRCH MAKECOLOR_5BIT_RGB(26, 26, 26)   // white
+#define CHERRY MAKECOLOR_5BIT_RGB(25, 12, 18)  // pink/lavender
+#define ELM MAKECOLOR_5BIT_RGB(25, 25, 0)      // yellow/orange
+
 namespace game {
 
 namespace player {
 
-static const Color colors_[] = {MAKECOLOR_5BIT_RGB(31, 25, 0), RED, BLUE, GREEN,
-                                MAGENTA};
+static const Color colors_[] = {GREEN, MAPLE, BIRCH, CHERRY, ELM};
 
 byte GetNext(byte current_player) {
   byte next_player = current_player + 1;
