e3080054d7335da6fb457e260c5985bc8a07cdc7
Bruno <bga@gmail.com>
2021-04-16 13:10:04 -0700

    Add mapping config file.

---
+++ config/mapping_config.h
@@ -0,0 +1,8 @@
+#ifndef MAPPING_CONFIG_H_
+#define MAPPING_CONFIG_H_
+
+// Maximum map size will be 16x16 (256 positions).
+#define MAPPING_WIDTH 16
+#define MAPPING_HEIGHT 16
+
+#endif  // MAPPING_CONFIG_H_
